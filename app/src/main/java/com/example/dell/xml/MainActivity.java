525b5fc5b195d15947578463def63a63d44fc02f
ankitjain10 <ankitjain.cse10@gmail.com>
2015-06-28 21:55:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/xml/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.dell.xml;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends ActionBarActivity {
+    EditText ed1,ed2,ed3,ed4,ed5,ed6,ed7;
+
+    private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private String url2 = "&mode=xml";
+    private HandleXML obj;
+    Button b1;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+        ed3=(EditText)findViewById(R.id.editText3);
+        ed4=(EditText)findViewById(R.id.editText4);
+        ed5=(EditText)findViewById(R.id.editText5);
+        ed6=(EditText)findViewById(R.id.editText6);
+        ed7=(EditText)findViewById(R.id.editText7);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ed1.getText().toString();
+                String finalUrl = url1 + url + url2;
+                ed2.setText(finalUrl);
+
+                obj = new HandleXML(finalUrl);
+                obj.fetchXML();
+
+                while(obj.parsingComplete);
+                ed2.setText(obj.getCountry());
+                ed3.setText(obj.getTemperature());
+                ed4.setText(obj.getHumidity());
+                ed5.setText(obj.getPressure());
+                ed6.setText(obj.getClouds());
+                ed7.setText(obj.getRain());
+                ed1.setText("");
+            }
+        });
+    }
+
+   }
